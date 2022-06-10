2455f849ffac7b125cfb0434fac176d9f7891130
Punitha Krishnan <punithakrishnan@Punithas-MBP.lan>
2022-06-10 17:40:28 -0400

    updated code

--- Mapping_GeoJSON_Points_1copy/static/js/logic.js
+++ Mapping_GeoJSON_Points_1copy/static/js/logic.js
@@ -5,7 +5,7 @@ console.log("working");
   //let map = L.map('map').setView([37.5, -122.5], 10);
 
   // Create the map object with center and zoom level.
-let map = L.map('map').setView([30, 30], 2);
+//let map = L.map('map').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -14,8 +14,33 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
     accessToken: API_KEY
 });
 
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with a center and zoom level.
+let map = L.map("map", {
+  center: [30, 30],
+  zoom: 4,
+  layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+//streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/Punithakrishnan/Mapping_Earthquakes/main/majorAirports.json" ;
@@ -38,6 +63,7 @@ L.geoJSON(data, {
 }).addTo(map);
 
 }); 
+
 
 
 
